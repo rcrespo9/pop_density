e190e9f9d7f2bb22e823ce97b4e9d43d83835d61
adammcc <ajm845@gmail.com>
2013-11-01 16:46:06 -0400

    db is seeded with data

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'mongoid', git: 'git://github.com/mongoid/mongoid.git'
 
 gem 'bson_ext'
 
+gem 'nokogiri'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+require 'nokogiri'
+require 'open-uri'
+
+Country.delete_all
+
+pop_data = Nokogiri::HTML(open("http://www.worldatlas.com/aatlas/populations/ctypopls.htm#.UnPycpTXRhQ"))
+pop_data_name = pop_data.css('tbody tr td:nth-child(2)')
+pop_data_density = pop_data.css('tbody tr td:nth-child(5)')
+pop_array_name = []
+pop_array_density = []
+pop_array_name.push pop_data_name
+pop_array_density.push pop_data_density
+
+
+
+i = 0
+while i < 192 do
+		Country.create(name: pop_array_name[0][i].text, pop_density: pop_array_density[0][i].text.to_i)
+	i += 1
+end
