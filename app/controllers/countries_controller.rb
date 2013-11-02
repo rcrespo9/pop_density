d21f32c55950bc617f4abe085a0daf4b7f1b33f6
adammcc <ajm845@gmail.com>
2013-11-02 14:33:13 -0400

    select country is king of working

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,5 +2,10 @@ class CountriesController < ApplicationController
 
 	def index
 		@countries = Country.all
+		# @country = Country.find(params[:country_id])
 	end
+
+	# def get_density
+	# 	@country = Country.find(params[:index])
+	# end
 end
